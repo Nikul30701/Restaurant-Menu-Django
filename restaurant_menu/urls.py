57b7a0d2a908a243c0bcbc147a2252c405cd9cbd
Nikul30701 <p.nikul6403@gmail.com>
2024-12-09 14:25:38 +0530

    Home page commit

---
+++ restaurant_menu/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from .views import MenuItemDetail
+
+urlpatterns = [
+    path("", views.MenuList.as_view(), name="home"),
+    path('item/<int:pk>/', MenuItemDetail.as_view(), name='menu_item')
+]
